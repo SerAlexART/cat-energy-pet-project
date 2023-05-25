9ea0dc9a7db4865859b642dfa10b838c795bb878
Sergey Alekseev <s_alekseev@lesta.group>
2023-05-25 17:17:29 +0300

    Стилизация страницы с формой

--- source/js/script.js
+++ source/js/script.js
@@ -12,3 +12,69 @@
 //     navMain.classList.remove('main-nav--opened');
 //   }
 // });
+
+
+
+
+
+
+// const pageBody = document.querySelector('.page__body');
+
+// const sliderCompare = document.querySelector('.slider-compare');
+
+// const sliderCompareBefore = sliderCompare.querySelector('.slider-compare__before');
+// const sliderCompareBeforeImage = sliderCompareBefore.querySelector('.slider-compare__image');
+
+// const sliderCompareAfter = sliderCompare.querySelector('.slider-compare__after');
+// const sliderCompareAfterImage = sliderCompareAfter.querySelector('.slider-compare__image');
+
+// const sliderCompareChange = sliderCompare.querySelector('.slider-compare__change');
+
+// let isActive = false;
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   let widthSliderCompareBeforeImage = sliderCompare.offsetWidth;
+//   sliderCompareBeforeImage.style.width = `${widthSliderCompareBeforeImage}px`
+// });
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   let widthSliderCompareAfterImage = sliderCompare.offsetWidth;
+//   sliderCompareAfterImage.style.width = `${widthSliderCompareAfterImage}px`
+// });
+
+// const beforeAfterSlider = (x) => {
+//   let shiftSlider = Math.max(0, Math.min(x, sliderCompare.offsetWidth));
+//   sliderCompareBefore.style.width = `${shiftSlider}px`
+//   sliderCompareChange.style.left = `${shiftSlider}px`
+// }
+
+// const pauseSlider = (e) => {
+//   e.stopPropagation();
+//   e.preventDefault();
+//   return false;
+// }
+
+// pageBody.addEventListener('mousedown', () => {
+//   isActive = true;
+// });
+
+// pageBody.addEventListener('mouseup', () => {
+//   isActive = false;
+// });
+
+// pageBody.addEventListener('mouseleave', () => {
+//   isActive = false;
+// });
+
+// pageBody.addEventListener('mousemove', (e) => {
+//   if (!isActive) {
+//     return;
+//   }
+
+//   let x = e.pageX;
+
+//   x -= sliderCompare.getBoundingClientRect().left;
+
+//   beforeAfterSlider(x);
+//   pauseSlider(e);
+// });
